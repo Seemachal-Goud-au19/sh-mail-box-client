f9ea3f087f941b33762ce7fceb3e617b03a783b7
Seemachal-Goud-au19 <simanchal9566@gmail.com>
2024-02-21 22:01:04 +0530

    Home page after login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Routes, Route, Navigate, Redirect } from 'react-router-dom';
 import AuthForm from './routes/Auth/AuthForm'
+import Home from './routes/Home/Home';
+import CartContext from './store/cart-context';
 
 const App = () => {
+  const cartCtx = useContext(CartContext)
   return (
     <>
       <Routes>
         <Route
           path="/login"
           element={<AuthForm />} />
+
+        {cartCtx.isLoggedIn && <Route
+          path="/"
+          element={<Home />} />}
       </Routes>
     </>
   )
